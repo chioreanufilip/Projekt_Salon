f9ce234ceda55725ca8bfd508a2570a3aca0e193
Chioreanu Filip-Andrei <chioreanufilip@gmail.com>
2024-11-08 12:24:15 +0200

    Employee and subclasses finished+Product

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-package src;
+//package src;
 import Module.NailPainter;
 import Module.Pedicurist;
 
 import java.util.Vector;
-public class Main {
+//public class Main {
 //    Vector<Barber> barbers = new Vector<>();
-    Vector<NailPainter> nailPainters = new Vector<>();
-    Vector<Pedicurist> pedicurists = new Vector<>();
-    public static void main(String[] args){
+//    Vector<NailPainter> nailPainters = new Vector<>();
+//    Vector<Pedicurist> pedicurists = new Vector<>();
+//    public static void main(String[] args){
 //        System.out.println("Hello World");
 //        Menu menu = new Menu();
 //        menu.menu();
-    }
+//    }
 
-}
+//}
